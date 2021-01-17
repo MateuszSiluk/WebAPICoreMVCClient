fbf8f3d392995fbdc93e093cbeb38a17c1a008ae
Mateusz Siluk <mati169@gmail.com>
2021-01-17 14:07:48 +0000

    Create the IOrderRepository Interface

---
+++ WebAPI/Models/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Models
+{
+    public interface IOrderRepository
+    {
+        IEnumerable<Order> GetAllOrders();
+        Order GetOrderById(int id);
+        Order AddOrder(Order order);
+        Order UpdateOrder(Order order);
+        void DeleteOrder(int id);
+
+    }
+}
