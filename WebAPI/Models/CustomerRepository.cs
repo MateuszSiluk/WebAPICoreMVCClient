fefc9adbbdfa1b7b9c7a2852cf885dff663a7021
Mateusz Siluk <mati169@gmail.com>
2021-01-18 16:50:24 +0000

    Completing Customer Repository CRUD Methods

--- WebAPI/Models/CustomerRepository.cs
+++ WebAPI/Models/CustomerRepository.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
@@ -60,24 +61,118 @@ namespace WebAPI.Models
             return customers;
         }
 
-        public Customer GetCustomerById(int Id)
+        public Customer GetCustomerById(int id)
         {
-            throw new NotImplementedException();
+            Customer customer = new Customer();
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("[dbo].[spSelectCustomerById]", con);
+                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    con.Open();
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    SqlDataReader rdr = cmd.ExecuteReader();
+                    while (rdr.Read())
+                    {
+                        customer.Id = id;customer.Name = rdr["Name"].ToString();
+                        customer.Address = rdr["Address"].ToString();
+                        customer.Telephone = rdr["Telephone"].ToString();
+                        customer.Email = rdr["Email"].ToString();
+                        
+
+                    }
+                    rdr.Close();
+                }
+                catch (Exception ex)
+                {
+
+                    _logger.LogError(ex, "Error at Get CustomerById()() : ");
+                    customer = null;
+
+                }
+            }
+            return customer;
         }
 
         public Customer AddCustomer(Customer customer)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("[dbo].[spInsertIntoCustomer]", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.Parameters.AddWithValue("@Name", customer.Name);
+                    cmd.Parameters.AddWithValue("@Address", customer.Address);
+                    cmd.Parameters.AddWithValue("@Telephone", customer.Telephone);
+                    cmd.Parameters.AddWithValue("@Email", customer.Email);
+                    // cmd.Parameters.AddWithValue("@ret", ParameterDirection.Output);
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    //ex.Message.ToString();
+                    _logger.LogError(ex, "Error at AddCustomer() :(");
+                    customer = null;
+                }
+
+            }
+            return customer;
         }
 
         public Customer UpdateCustomer(Customer customer)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("[dbo].[spUpdateCustomer]", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.Parameters.AddWithValue("@Id", customer.Id);
+                    cmd.Parameters.AddWithValue("@Name", customer.Name);
+                    cmd.Parameters.AddWithValue("@Address", customer.Address);
+                    cmd.Parameters.AddWithValue("@Telephone", customer.Telephone);
+                    cmd.Parameters.AddWithValue("@Email", customer.Email);
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    //ex.Message.ToString();
+                    _logger.LogError(ex, "Error at UpdateCustomer() :(");
+                    customer = null;
+                }
+            }
+
+            return customer;
         }
 
         public void DeleteCustomer(int id)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                try
+                {
+                    SqlCommand cmd = new SqlCommand("[dbo].[spDeleteCustomer]", connection);
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    connection.Open();
+                    cmd.Parameters.AddWithValue("@Id", id);
+                    cmd.ExecuteNonQuery();
+
+                }
+                catch (Exception ex)
+                {
+                    //ex.Message.ToString();
+                    _logger.LogError(ex, "Error at DeleteCustomer() :(");
+
+                }
+
+            }
         }
     }
 }
