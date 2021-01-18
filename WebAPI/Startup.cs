2fddfdee057aaaee6207c8db421f6355f3d912b2
Mateusz Siluk <mati169@gmail.com>
2021-01-18 17:26:52 +0000

    Registering in Startup Class

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -10,6 +10,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebAPI.Models;
 
 namespace WebAPI
 {
@@ -25,6 +26,9 @@ namespace WebAPI
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<ICustomerRepository, CustomerRepository>();
+            services.AddSingleton<IOrderRepository, OrderRepository>();
+
             services.AddRazorPages()
         .AddRazorRuntimeCompilation();
         }
